d8607989b0e82730c7da2d665def49049cc979a9
vklee88 <vklee88@gmail.com>
2018-11-03 15:33:44 -0700

    server code

--- server/app.py
+++ server/app.py
@@ -1,13 +1,24 @@
-from flask import Flask, request
+import os
+from flask import Flask, request, send_from_directory, send_file
 from flask_socketio import SocketIO
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build')
 socketio = SocketIO(app)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    app.send_static_file('index.js')
+    return send_from_directory('build', 'index.html')
+
+
+@app.route('/static/<path:path>')
+def send_static_file(path):
+    return send_from_directory('build/static', path)
+
+
+@app.route('/manifest.json')
+def send_manifest():
+    return send_file('build/manifest.json')
 
 
 @socketio.on('connect')
@@ -17,10 +28,14 @@ def on_create():
 
 @socketio.on('data')
 def on_data(data):
-    # run ML code
-    socket_id = request.namespace.socket.sessid
+    # image is in base64 and starts with "data:image/jpeg;base64,<image_data>"
+    socket_id = request.sid
     print('Socket ID:', socket_id)
+    f = list(data)
+    print(len(f))
     print('Data:', data)
+    # run ML code
+    return 'ack'
 
 
 @socketio.on('disconnect')
@@ -29,4 +44,4 @@ def on_leave():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ server/build/precache-manifest.6bbd0f861f50f3ee2620b0657cb7cb18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a093fd2b065b45965429",
+    "url": "/static/js/main.a093fd2b.chunk.js"
+  },
+  {
+    "revision": "7e1e6a334ddf5aeb644d",
+    "url": "/static/js/1.7e1e6a33.chunk.js"
+  },
+  {
+    "revision": "a093fd2b065b45965429",
+    "url": "/static/css/main.619c1cba.chunk.css"
+  },
+  {
+    "revision": "4b444ddfc66aa43d315bb01d8597ffb0",
+    "url": "/index.html"
+  }
+];
