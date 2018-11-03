98ffbf20d9dd5c1c6c6806836278fb6465b1bb5f
vklee88 <vklee88@gmail.com>
2018-11-03 11:15:07 -0700

    moved ML under server, updated server code

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
@@ -10,25 +10,23 @@ def index():
     app.send_static_file('index.js')
 
 
-@socketio.on('create')
-def on_create(data):
-    print(data)
-
-
-@socketio.on('join')
-def on_join(data):
-    print(data)
+@socketio.on('connect')
+def on_create():
+    print('Client connected')
 
 
 @socketio.on('data')
 def on_data(data):
     # run ML code
-    print(data)
+    socket_id = request.namespace.socket.sessid
+    print('Socket ID:', socket_id)
+    print('Data:', data)
 
 
-@socketio.on('leave')
-def on_leave(data):
-    print(data)
+@socketio.on('disconnect')
+def on_leave():
+    print('Client disconnected')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
