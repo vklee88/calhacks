fe88272bce1eef0631de4fe866b9b7776af37f1d
vklee88 <vklee88@gmail.com>
2018-11-03 00:09:02 -0700

    Frontend complete, data sent to server as a webm video file

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import MediaStreamRecorder from 'msr';
 import io from 'socket.io-client';
 import './App.css';
 
@@ -7,60 +6,79 @@ class App extends Component {
   BROWSER_ERR = "You seem to using a browser that does not support video. " +
                 "Please don't use IE or upgrade your browser.";
   PERMISSION_ERR = "You need to allow access to a camera to run this app.";
+  URL = 'http://localhost:5000';
 
   constructor(props) {
     super(props);
     this.videoRef = React.createRef();
     this.state = {
-      err: null
+      err: null,
+      mediaRecorder: null,
+      socket: null,
+      storedStream: null
     };
-    this.mediaRecorder = null;
-    this.socket = null;
-    this.storedStream = null;
+
+    this.uploadData = this.uploadData.bind(this);
   }
 
   startRecording() {
     let video = this.videoRef.current;
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({video: true})
-        .then(stream => {
-          this.socket = io('ws://localhost');
-          socket.on('connect', () => {
-            this.setState({err: null});
+      let socket = io(this.URL);
+      this.setState({socket: socket});
+      socket.on('connect', () => {
+        navigator.mediaDevices.getUserMedia({video: true})
+          .then(stream => {
             video.src = window.URL.createObjectURL(stream);
-            this.storedStream = stream;
-            this.mediaRecorder = new MediaStreamRecorder(stream);
-            this.mediaRecorder.mimeType = 'video/webm';
-            this.mediaRecorder.ondataavailable(this.uploadData);
+            // TODO MediaRecorder is a firefox/chrome only feature
+            let mediaRecorder = new MediaRecorder(stream, {
+              mimeType : 'video/webm'
+            });
+            mediaRecorder.start(1000);
+            mediaRecorder.ondataavailable = this.uploadData;
+            this.setState({err: null, storedStream: stream, mediaRecorder: mediaRecorder});
+          })
+          .catch(err => {
+            let message = err.message;
+            if (err.name === "PermissionDeniedError") {
+              message = this.PERMISSION_ERR;
+            }
+            this.setState({err: message});
           });
-        })
-        .catch(err => {
-          let message = err.message;
-          if (err.name === "PermissionDeniedError") {
-            message = this.PERMISSION_ERR;
-          }
-          this.setState({err: message});
-        });
+      });
+      socket.on('connect_timeout', () => {
+        alert('Timeout when connecting to server');
+        this.state.socket.close();
+        this.setState({socket: null});
+      });
     } else {
       this.setState({err: this.BROWSER_ERR});
     }
   }
 
   stopRecording() {
-    if (this.storedStream) {
-      if (this.socket) {
-        this.socket.close();
-        this.socket = null;
-      }
-      for (let track of this.storedStream.getTracks()) {
+    if (this.state.socket) {
+      this.state.socket.close();
+      this.setState({socket: null});
+    }
+    if (this.state.storedStream) {
+      for (let track of this.state.storedStream.getTracks()) {
         track.stop();
       }
-      this.storedStream = null;
+      this.setState({storedStream: null});
+    }
+    if (this.state.mediaRecorder) {
+      this.state.mediaRecorder.stop();
+      this.setState({mediaRecorder: null});
     }
   }
 
-  uploadData(blob) {
-    this.socket.emit('data', blob);
+  uploadData(blobEvent) {
+    console.log(blobEvent);
+    console.log(this.state.socket);
+    if (this.state.socket) {
+      this.state.socket.emit('data', blobEvent.data);
+    }
   }
 
   render() {
